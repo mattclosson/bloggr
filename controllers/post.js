3be83dd895c6e0d1a0441a8f54035a9d2f135f8c
Matt Closson <mattwclosson@gmail.com>
2021-12-28 14:13:26 -0500

    fixed signup page

--- controllers/post.js
+++ controllers/post.js
@@ -5,46 +5,5 @@ const mongoose = require("../models/connection")
 
 const router = express.Router()
 
-// index route - get - /posts
-router.get("/:username/:slug", (req, res, next) => {
-    const slug = req.params.slug
-    const username = req.params.username
-  
-    User.findOne({username: username})
-    .populate('posts')
-    .then((post) => {
-      console.log(post)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  
-    Post.findOne({slug})
-    .then((post) => {
-        res.render("posts/show.liquid", {post})
-    })
-    .catch((err) => {
-        res.json({err})
-    })
-  })
-  
-  // index route - get - /posts
-  router.get("/:username", async (req, res, next) => {
-    const username = req.params.username
-    const user = await User.findOne({username})
-  
-    if(user) {
-        const id = user._id
-        Post.find({author: id})
-        .then((posts) => {
-          res.render("posts/index.liquid", {posts, user})
-        })
-        .catch((err) => {
-          res.json(err)
-        })
-    } else {
-        next()
-    }
-  })
 
 module.exports = router
